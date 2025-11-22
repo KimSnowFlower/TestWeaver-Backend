4192445921d8946f184e2917a508a0b061b1949c
KimSnowFlower <kimbona1148@gmail.com>
2025-11-22 13:09:04 +0900

    fix: #21 update async Handler error code

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,4 @@
-module.export = function asyncHandler(fn) {
+module.exports = function asyncHandler(fn) {
     return (req, res, next) => {
         Promise.resolve(fn(req, res, next)).catch(next);
     }
